f95052a8b0c05e8ebf158b2169cfec57ba9dc5b5
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:11:51 -0400

    Add Browser/BrowserTests/BrowserTests.swift

---
+++ Browser/BrowserTests/BrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserTests.swift
+//  BrowserTests
+//
+//  Created by Elliot Williams on 2025-06-29.
+//
+
+import Testing
+@testable import Browser
+
+struct BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
